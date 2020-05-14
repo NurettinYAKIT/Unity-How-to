42a1a7c038014c0413e9830c0d7417efd1989153
onuryakit <nurettinonur.yakit@jumbo.com>
2020-05-14 21:33:30 +0200

    check point

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float delay = 2f;
+    bool gameHasEnded = false;
+    public GameObject completeLevelUI;
+
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", delay);
+        }
+
+    }
+    public void CompleteLevel(){
+        completeLevelUI.SetActive(true);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float forwardForce = 1200f;
+    public float sideWayforce = 50f;
+
+    private float verticalSpeed;
+    private float horizontalSpeed;
+
+    void Update()
+    {
+        verticalSpeed = Input.GetAxis("Vertical") * forwardForce * Time.deltaTime;
+        horizontalSpeed = Input.GetAxis("Horizontal") * sideWayforce * Time.deltaTime;
+    }
+
+    // FixedUpdate is better when you are working with pyhcics.
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(0, 0, verticalSpeed);
+        rigidbody.AddForce(horizontalSpeed, 0, 0,ForceMode.VelocityChange);
+
+        if(rigidbody.position.y<-1f){
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+    private void oldWay()
+    {
+        if (Input.GetKey("w"))
+        {
+            rigidbody.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(-sideWayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rigidbody.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rigidbody.AddForce(sideWayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
